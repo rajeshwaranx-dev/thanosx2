0bb022a06b779e7d0027bb82f81f95bfcb82ea21
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-04-30 18:05:09 +0530

    Ask Botz❤️‍🔥

--- Script.py
+++ Script.py
@@ -144,7 +144,7 @@ class script(object):
 
 <b>‣ ꜱɪᴢᴇ : </b>{file_size}
 
-<b>🍁 ʙʏ : <a href="https://telegram.me/Thanos_linkz">[ Thanks Linkz ]</a></b>"""
+<b>🍁 ʙʏ : <a href="https://telegram.me/Thanos_linkz">[ Thanos Linkz ]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """<b><a href={url}>{title} (<a href={url}/releaseinfo>{year}</a>)
 
